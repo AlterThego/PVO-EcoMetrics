ca76bfb251b9b6012cd221105ce265070b0b4907
AlterThego <theosuaking@gmail.com>
2024-01-22 14:28:03 +0800

    Modified Model, Factory and added Seeder for User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50);
+            $table->unsignedBigInteger('municipality_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['admin', 'user', 'municipality_user']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
