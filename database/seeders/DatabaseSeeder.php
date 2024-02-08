f1667573697594e7a72b72e39380eab41187467c
Duriel A. Timatim <durieltims@gmail.com>
2024-02-08 11:32:54 +0800

    Updated Seeder and Package.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -37,7 +41,17 @@ class DatabaseSeeder extends Seeder
         $this->call(YearlyCommonDiseaseSeeder::class);
 
         // Enable to test User
-        // $this->call(UserSeeder::class); 
-    }
+        $this->call(UserSeeder::class);
 
+        //admin user 
+        $admin = DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => "admin@benguet.gov.ph",
+            'email_verified_at' => now(),
+            'municipality_id' => null,
+            'role' => "admin",
+            'password' =>  Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
 }
